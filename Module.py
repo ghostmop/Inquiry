d5a1bbed6d103e678b29cfeb11a6f605c57b2adb
Michael <michaesunsan@users.noreply.github.com>
2015-04-01 19:59:50 -0400

    print line

--- Module.py
+++ Module.py
@@ -1,7 +1,7 @@
+print "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
 
 
 
 
 
-
-raw_input("Press Enter to exit")
+raw_input("What would you like to do?")
