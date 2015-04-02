c7afe5fba17f88905bf0334e0567a83930225b30
Michael <michaesunsan@users.noreply.github.com>
2015-04-01 20:19:42 -0400

    objects??

--- Module.py
+++ Module.py
@@ -1,10 +1,16 @@
-print "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
+location_0_description = "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
+print location_0_description
 
 location = 0
-while location = 0:
+while location == 0:
 	if raw_input("What would you like to do? ") == "look":
-		print "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
+		print location_0_description
+	#elif raw_input("What would you like to do? ") == "look":
+		#print ""
 	else:
 		print "That is not a recognized command."
 
 raw_input("Press Enter to exit.")
+
+
+
