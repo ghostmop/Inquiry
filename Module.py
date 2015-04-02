0d189928573536219d1d17cfbd39486dc864fd14
Michael <michaesunsan@users.noreply.github.com>
2015-04-01 20:03:22 -0400

    Hope this works

--- Module.py
+++ Module.py
@@ -1,7 +1,7 @@
 print "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
+if raw_input("What would you like to do?") == "look":
+	print "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
+else:
+	print "That is not a recognized command."
 
-
-
-
-
-raw_input("What would you like to do?")
+raw_input("Press Enter to exit.")
