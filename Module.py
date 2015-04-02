48dbae7f00ec8ee8d0e864fbfed13a697f2b94a5
Michael <michaesunsan@users.noreply.github.com>
2015-04-01 20:11:55 -0400

    what's wrong

--- Module.py
+++ Module.py
@@ -1,7 +1,10 @@
 print "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
-if raw_input("What would you like to do?") == "look":
-	print "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
-else:
-	print "That is not a recognized command."
+
+location = 0
+while location = 0:
+	if raw_input("What would you like to do? ") == "look":
+		print "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
+	else:
+		print "That is not a recognized command."
 
 raw_input("Press Enter to exit.")
