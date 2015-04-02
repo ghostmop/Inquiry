0b5daa40b1662663d5ada0f323b2bb8356e606a6
Michael <michaesunsan@users.noreply.github.com>
2015-04-01 20:50:53 -0400

    deleting the test documents

--- Sublime Text Testing.py
+++
@@ -1,8 +0,0 @@
-print "I hope this works!"
-
-print "I hope this works"
-print "Ahh this better work"
-print "hi"
-print "hello"
-print "You know, it would be great if this thing worked"
-print "Testing one two three"
