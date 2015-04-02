2957559235227f884cd663342d4c16ffbc234ea5
myloxyloto1 <isabel.ng@rogers.com>
2015-04-01 20:30:28 -0400

    added descriptions

--- Module.py
+++ Module.py
@@ -1,6 +1,12 @@
 location_0_description = "Description of the location, including things to look at, people to talk to, and other locations to go to (and their directions)"
 print location_0_description
 
+object_1_description = "Description of object 1"
+print object_1_description
+
+object_2_description = "Description of object 2"
+print object_2_description
+
 location = 0
 while location == 0:
 	if raw_input("What would you like to do? ") == "look":
