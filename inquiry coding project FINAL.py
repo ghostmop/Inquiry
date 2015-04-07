837f5bf0dcbd39009b548556184614e93d729cfd
Michael <michaesunsan@users.noreply.github.com>
2015-04-07 14:56:10 -0400

    Final beta version! should be fully functioning but not entirely polished

--- inquiry coding project FINAL.py
+++ inquiry coding project FINAL.py
@@ -136,6 +136,8 @@ while True:	#location 0
 
 	Attendant_dialogue = "The girl attending the stand smiles as she asks, "
 
+	if location == 4:
+		print "You are now outside."
 	while location == 4:
 		user_input = raw_input("What would you like to do? ")
 		if user_input == "look":
@@ -164,7 +166,7 @@ while True:	#location 0
 
 	location_6_description = "You find yourself inside of MDCL. Behind you lies the Outdoors, and ahead of you is HSC. You wonder what Tim Horton\'s has on their menu today. You also happen to see one of your good friends at the cashier with a large cup of coffee in hand."
 	if location == 6:
-		print location_6_description
+		print "You are inside MDCL."
 
 	menu_description = "The menu reads Coffee : $1.60 (Medium) ; Donut : $0.99 ; Muffin : $1.29 ; Cookie : $0.99 ; 10-pack Timbits : $2.30 ; Cheesy Pizza : $5.00 ; Mac and Cheese : $4.00 (Large)."
 
@@ -193,6 +195,49 @@ while True:	#location 0
 		else: 
 			print "That is not a valid command."
 
-	else:
-		raw_input("Uh-oh.")
-		break
+
+	location_7_description = "You are now inside of the Health Sciences Centre...Home of the Health Scis! Behind you is MDCL, and to your right is the Health Sci Lounge. Walking up the main flight of stairs, you notice a new sign placed outside of the Health Sciences Libary. You also notice a peculiar man standing outside of the library entrance."
+
+	sign_description = "The sign reads: April 8th, 3:00 - 5:00 pm --- Dr. P.K. Rangachari, Professor (Emeritus) of Medicine, will be discussing his philosophy of teaching and his strategies for engaging students. All are welcome to attend. Refreshments will be served. Hmm...the \"I\" at the end of Chari's name is kind of crooked, you notice."
+
+	man = "The peculiar man sees you approach. Why hello there! I'm a new prof in the Department of Pharmacology and Toxicology. *At this, the prof glances down at the library sign.* Did you hear the news? Dr. P.K. Rangachari will be giving a talk, right here in the library. Will you be attending?"
+	yes_answer = "I'm glad. It's nice to see that young people are actually interested in attending these sorts of events. What's that? You say that Dr.Rangachari is your Cellular Biology professor? My, what a lucky student! I had the pleasure of working with Dr.Rangachari myself once...together, we published an article discussing how low doses of ionizing radiation can prevent radiation-induced colonic epithelial hyporesponsiveness to muscarnic agonists and...You zone out as this new prof keeps talking. The way he pronounces the \"C\" in Chari's name is particularly amusing, though."
+	no_answer = "WHAT?! Don't be ridiculous! You really must attend, I mean, it is Dr. RANGACHARI that is speaking. THE Dr. RANGACHARI! How dare you not even consider attending an event in which he, The GREAT RANGACHARI, will be speaking. This goes to show just how self-absorbed and narrow-minded the youth of today are...I am absolutely disgusted with the world. My, the world must be ending! Yes, that's it! The world is -- at this, you slowly start to back away. The way be pronounced the \"C\" in Chari's name was particularly amusing, though."
+	if location == 7:
+		print "You are now in HSC!"
+	while location == 7:
+		user_input = raw_input("What would you like to do? ")
+		if user_input == "look":
+			print location_7_description
+		elif user_input == "look sign":
+			print sign_description
+		elif user_input == "talk" or user_input == "talk man":
+			answer_q1 = raw_input(man)
+			if answer_q1.lower() == "yes":
+				print yes_answer
+			elif answer_q1.lower() == "no":
+				print no_answer
+			else:
+				print "I'm sorry, I'm a little hard of hearing. Could you repeat that please?"
+		elif user_input == "back":
+			location = 6
+		elif user_input == "right":
+			location = 8
+		else:
+			print "That is not a valid command."
+
+	location_8_description = "You now stand in front of the Health Sci Lounge. Behind you is the HSC lobby. You try to unlock the lounge door using your Health Sci access card, as you've always done, but the door doesn't budge. You notice a note with tiny writing taped to the door. It reads: Well, brave voyageur, looks like you've finally reached the lounge. Did you notice any strange looking letters along the way? Unscramble those letters and enter in your final answer in the keypad below. Answer correctly, and you will gain access to the lounge. Answer incorrectly, and you will face certain death. It's really that simple."
+	congrats_message = "You hear the reassuring sound of a lock clicking. Slowly, you pull the handle of the door...and it opens! CONGRATULATIONS! You made it to the lounge in one piece! Thanks for playing!"
+
+	while location == 8:
+		user_input = raw_input("Enter the secret passcode below. Do not use spaces.")
+		if user_input.lower() == "hcihywylt":
+			print congrats_message
+			break
+		elif user_input == "back":
+			location = 7
+		else:
+			print "I'm sorry, but that is not the correct passcode. Please try again."
+	break
+
+raw_input("Press enter to accept the praise of your peers.")
